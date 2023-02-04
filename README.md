6446e169dff9c59daac8fbd0929eaca133633d80
Luke Davis <luke@lukealexdavis.co.uk>
2023-02-04 14:58:53 +0000

    uploaded main files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bandcamp_calc
- 
+# Bandcamp Calculator
+
+This is a Python script that returns the running time of an album from Bandcamp.
+
+# How to use
+
+In your command line, enter `python3 bandcamp_calc.py -u [URL]` and it will print out the running time in seconds.
---
+++ bandcamp_calc.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import argparse
+
+# Create CL argument for URLs
+parser = argparse.ArgumentParser(description=None)
+parser.add_argument("-u", "--url", type=str)
+args = parser.parse_args()
+url = args.url
+
+# Empty list for track lengths
+tm = []
+
+# Extract times from release page
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
+times_list = soup.find_all('span', attrs={'class':'time secondaryText'})
+cleaned_up_times_list = [time.text.replace('\n','').replace(' ','') for time in times_list]
+
+# Convert and add times to `tm` list
+for t in cleaned_up_times_list:
+	(m, s) = t.split(':')
+	d = datetime.timedelta(minutes=int(m), seconds=int(s))
+	tm.append(d)
+
+# Sum and print total length
+total_time = sum(tm, start=datetime.timedelta())
+print(total_time)
